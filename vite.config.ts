6534ed57d1e6423264b9d2d2335a896011131576
eeegg <hgbhh258@gmail.com>
2026-06-19 04:12:53 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/Sonic-Topography-Wallpaper/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
